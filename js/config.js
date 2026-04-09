287ed42510a8f936f253031f8394c38885669e73
stupakzm <zaharstupak657@gmail.com>
2026-04-09 12:18:25 +0200

    feat: add static config (contact info, data URL)

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  // Contact info shown in modal and footer
+  phone: '+48 000 000 000',
+  email: 'contact@firm.com',
+
+  // Path to generated jobs data file
+  jobsDataUrl: 'data/jobs.json',
+}
